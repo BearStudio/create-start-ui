bf17f2d8b4f540c7c8de6c39c59616e39fa32b11
Renan Decamps <renan.decamps@gmail.com>
2025-04-25 17:15:59 +0200

    feat: update sentry with sampleRate and update github action

--- src/lib/sentry.ts
+++ src/lib/sentry.ts
@@ -6,6 +6,8 @@ export const sentry = Sentry.init({
   dsn: 'https://2512a71f28fa9372bce8824d1ccc038c@sentry.bearstudio.info/4',
   release: packageJson.version,
 
+  tracesSampleRate: 1.0,
+
   // Make sure no events will be sent to sentry
   // if telemetry is disabled
   beforeSend: (event) => {
